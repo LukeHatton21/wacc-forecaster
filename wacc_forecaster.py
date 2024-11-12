0cfc1bede571a89ac73d07b8daf81d5f85c3e149
LukeHatton21 <92597045+LukeHatton21@users.noreply.github.com>
2024-11-12 17:21:53 +0000

    edits

--- wacc_forecaster.py
+++ wacc_forecaster.py
@@ -1,26 +1,26 @@
 import streamlit as st
 import folium
 import pandas as pd
+import numpy as np
 from streamlit_folium import st_folium
 import branca.colormap as cm
 from wacc_prediction import WaccPredictor
+import altair as alt
 
 # Data Source for map: https://public.opendatasoft.com/explore/embed/dataset/world-administrative-boundaries-countries/table/
 
+# Call WaccPredictor Object
+wacc_predictor = WaccPredictor("./DATA/CRPs.csv", "./DATA/Ember Yearly Data 2023.csv", "./DATA/GDPPerCapita.csv","./DATA/TaxData.csv", "./DATA/OECD_IR.csv")
+
+
 def display_map(df, technology):
     map = folium.Map(location=[10, 0], zoom_start=1, control_scale=True, scrollWheelZoom=True, tiles='CartoDB positron')
-    
-    if technology == "Solar PV":
-        row = "solar_pv_wacc"
-    elif technology == "Onshore Wind":
-        row = "onshore_wacc"
-    elif technology == "Offshore Wind":
-        row = "offshore_wacc"
+    df = df.rename(columns={"Country code":"iso3_code"})
 
     choropleth = folium.Choropleth(
         geo_data='./DATA/country_boundaries.geojson',
         data=df,
-        columns=('iso3_code', row),
+        columns=('iso3_code', "WACC"),
         key_on='feature.properties.iso3_code',
         line_opacity=0.8,
         highlight=True,
@@ -40,17 +40,23 @@ def display_map(df, technology):
     #choropleth.color_scale.width = 10
 
     df_indexed = df.set_index('iso3_code')
-    df_indexed = df_indexed.dropna(subset="onshore_wacc")
+    df_indexed = df_indexed.dropna(subset="WACC")
     for feature in choropleth.geojson.data['features']:
         iso3_code = feature['properties']['iso3_code']
-        feature['properties']['Solar WACC'] = (
-        f"{df_indexed.loc[iso3_code, 'solar_pv_wacc']:0.2f}%" if iso3_code in df_indexed.index else "N/A"
+        feature['properties'][technology + ' WACC'] = (
+        f"{df_indexed.loc[iso3_code, 'WACC']:0.2f}%" if iso3_code in df_indexed.index else "N/A"
     )
-        feature['properties']['Onshore Wind WACC'] = (
-        f"{df_indexed.loc[iso3_code, 'onshore_wacc']:0.2f}%" if iso3_code in df_indexed.index else "N/A"
+        feature['properties']["Debt_Share"] = (
+        f"{df_indexed.loc[iso3_code, 'Debt_Share']:0.2f}%" if iso3_code in df_indexed.index else "N/A"
     )
-        feature['properties']['Offshore Wind WACC'] = (
-        f"{df_indexed.loc[iso3_code, 'offshore_wacc']:0.2f}%" if iso3_code in df_indexed.index else "N/A"
+        feature['properties']["Equity_Cost"] = (
+        f"{df_indexed.loc[iso3_code, 'Equity_Cost']:0.2f}%" if iso3_code in df_indexed.index else "N/A"
+    )
+        feature['properties']["Debt_Cost"] = (
+        f"{df_indexed.loc[iso3_code, 'Debt_Cost']:0.2f}%" if iso3_code in df_indexed.index else "N/A"
+    )
+        feature['properties']["Tax_Rate"] = (
+        f"{df_indexed.loc[iso3_code, 'Tax_Rate']:0.2f}%" if iso3_code in df_indexed.index else "N/A"
     )
         #feature['properties']['GDP'] = 'GDP: ' + '{:,}'.format(df_indexed.loc[country_name, 'State Pop'][0]) if country_name in list(df_indexed.index) else ''
 
@@ -60,8 +66,8 @@ def display_map(df, technology):
 
     choropleth.geojson.add_child(
     folium.features.GeoJsonTooltip(
-        fields=['english_short', 'Solar WACC', 'Onshore Wind WACC', 'Onshore Wind WACC'],  # Display these fields
-        aliases=["Country:", "Solar PV:", "Onshore Wind\n:", "Offshore Wind:"],         # Display names for the fields
+        fields=['english_short', technology + ' WACC', "Equity_Cost", "Debt_Cost", "Debt_Share", "Tax_Rate"],  # Display these fields
+        aliases=["Country:", technology + ":", "Cost of Equity:", "Cost of Debt:", "Debt Share:", "Tax_Rate"],         # Display names for the fields
         localize=True,
         style="""
         background-color: #F0EFEF;
@@ -80,26 +86,186 @@ def display_map(df, technology):
         country_name = st_map['last_active_drawing']['properties']['english_short']
     return country_name
 
+@st.cache_data
+def get_sorted_waccs(df, technology):
+
+    if technology == "Solar PV":
+        column = "solar_pv_wacc"
+    elif technology == "Onshore Wind":
+        column = "onshore_wacc"
+    elif technology == "Offshore Wind":
+        column = "offshore_waccs"
+
+    sorted_df = df.sort_values(by=column, axis=0, ascending=True)
+    list = ["solar_pv_wacc", "onshore_wacc", "offshore_wacc"]
+    for columns in list:
+        if columns == column:
+            list.remove(columns)
+    sorted_df = sorted_df.drop(labels=list, axis="columns")
+    sorted_df = sorted_df.dropna(subset=column)
+    sorted_df = sorted_df.rename(columns={column:"WACC"})
+    sorted_df["WACC"] = sorted_df["WACC"].round(decimals=2)
+
+    return sorted_df
+
+def sort_waccs(df):
+
+    sorted_df = df.sort_values(by="WACC", axis=0, ascending=True)
+    list = ["WACC", "Equity_Cost", "Debt_Cost", "Debt_Share", "Tax_Rate"]
+    sorted_df = sorted_df.drop(labels=list, axis="columns")
+    
+    return sorted_df
+
+
+@st.cache_data
+def get_selected_country(df, country_code):
+
+    selected_wacc = df[df['Country code'] == country_code]
+
+    return selected_wacc
+
+
+def plot_ranking_table(raw_df, country_codes):
+
+    # Select countries
+    df = raw_df[raw_df["Country code"].isin(country_codes)]
+
+    
+    # Melt dataframe
+    df = df.rename(columns={"Risk_Free":" Risk Free", "Country_Risk":"Country Risk", "Technology_Risk":"Technology Risk"})
+    data_melted = df.melt(id_vars="Country code", var_name="Factor", value_name="Value")
+
+    # Set order
+    category_order = [' Risk Free', 'Country Risk', 'Equity Risk', 'Lenders Margin', 'Technology Risk']
+
+    # Create chart
+    chart = alt.Chart(data_melted).mark_bar().encode(
+        x=alt.X('sum(Value):Q', stack='zero', title='Weighted Average Cost of Capital (%)'),
+        y=alt.Y('Country code:O', sort="x", title='Country'),  # Sort countries by total value descending
+        color=alt.Color('Factor:N', title='Factor'),
+        order=alt.Order('Factor:O', sort="ascending"),  # Color bars by category
+).properties(width=700)
+
+    # Add x-axis to the top
+    x_axis_top = chart.encode(
+        x=alt.X('sum(Value):Q', stack='zero', title='Weighted Average Cost of Capital (%)', axis=alt.Axis(orient='top'))
+    )
+
+    # Combine the original chart and the one with the top axis
+    chart_with_double_x_axis = alt.layer(
+        chart,
+        x_axis_top
+    )
+
+    st.write(chart_with_double_x_axis)
+
+def plot_comparison_chart(df):
+   # Melt dataframe
+    df = df.rename(columns={"Risk_Free":" Risk Free", "Country_Risk":"Country Risk", "Technology_Risk":"Technology Risk"})
+    data_melted = df.melt(id_vars="Year", var_name="Factor", value_name="Value")
+
+    # Set order
+    category_order = [' Risk Free', 'Country Risk', 'Equity Risk', 'Lenders Margin', 'Technology Risk']
+
+    # Create chart
+    chart = alt.Chart(data_melted).mark_bar().encode(
+        x=alt.X('sum(Value):Q', stack='zero', title='Weighted Average Cost of Capital (%)'),
+        y=alt.Y('Year:O', title='Country'),  # Sort countries by total value descending
+        color=alt.Color('Factor:N', title='Factor'),
+        order=alt.Order('Factor:O', sort="ascending"),  # Color bars by category
+).properties(width=700)
+    st.write(chart)
+    
+
 
 country_waccs = pd.read_csv("./DATA/Country_Waccs_2024.csv")
-print(country_waccs)
+
 
 
 st.title("	🏦 Weighted Average Cost of Capital Forecaster (WACFOR)")
 st.write(
     "Tool to estimate the cost of capital for renewable technologies\nV1.0 - Alpha\n")
-tab1, tab2, tab3, tab4 = st.tabs(["🥇Global Estimates", "🌐 Map", "📈 Country Projections", "ℹ️ About"])
+year = st.selectbox(
+        "Year", ("2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023"), 
+         index=8, key="Year", placeholder="Select Year...")
+technology = st.selectbox(
+        "Displayed Technology", ("Solar PV", "Onshore Wind", "Offshore Wind"), 
+         index=0, placeholder="Select Technology...", key="Technology")
+tab1, tab2, tab3, tab4 = st.tabs(["🌐 Map", "🥇Global Estimates", "📈 Country Projections", "ℹ️ About"])
+
+
+# Calculate yearly results for solar, onshore and offshore
+yearly_waccs_solar = wacc_predictor.calculate_historical_waccs(year, "Solar PV")
+yearly_waccs_onshore = wacc_predictor.calculate_historical_waccs(year, "Onshore Wind")
+yearly_waccs_offshore = wacc_predictor.calculate_historical_waccs(year, "Offshore Wind")
+
+
+# Select specified technology for plotting
+if technology == "Solar PV":
+    yearly_waccs = yearly_waccs_solar
+elif technology == "Onshore Wind":
+    yearly_waccs = yearly_waccs_onshore
+elif technology == "Offshore Wind":
+    yearly_waccs = yearly_waccs_offshore
+
+
 
 with tab1:
-    st.header("Global Estimates")
-with tab2:
     st.header("Map")
-    option = st.selectbox(
-        "Displayed Technology", ("Solar PV", "Onshore Wind", "Offshore Wind"), 
-         index=0, placeholder="Select Technology...")
-    display_map(country_waccs, option)
+    display_map(yearly_waccs, technology)
+with tab2:
+    st.header("Global Estimates")
+    selected_countries = st.multiselect("Countries to compare", options=yearly_waccs['Country code'].values, default=["USA", "IND", "GBR", "JPN", "CHN", "BRA"])
+    sorted_waccs = sort_waccs(yearly_waccs)
+    plot_ranking_table(sorted_waccs, selected_countries)
+
 with tab3:
     st.header("Country Projections")
+    country_code = st.selectbox(
+        "Country", sorted_waccs['Country code'].sort_values(ascending=True).values, 
+         index=0, placeholder="Select Country...", key="Country")
+    projection_year = st.selectbox(
+        "Year", np.arange(2024, 2050, 1), 
+         index=6, placeholder="Select Year...", key="Projection Year")
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        st.subheader("Macro Environment")
+        rf_rate = st.number_input("Risk-free Rate (%)", value=2.5, min_value=1.0, max_value=10.0, step=0.1)
+        crp = st.number_input("Country Risk Premium (%)", value=5.0, min_value=0.0, max_value=20.0, step=0.1)
+    with col2:
+        st.subheader("Renewable Development")
+        market_maturity = st.selectbox(
+        "Market Maturity",["Mature", "Intermediate", "Immature"], 
+         index=2, placeholder="Select Maturity...", key="Maturity")
+        tech_penetration = st.number_input("Renewable Penetration (%)", value=None, min_value=0.0, max_value=30.0, step=1.0)
+    with col3:
+        st.subheader("Debt-Equity Premiums")
+        st.write("")
+        st.write("")
+        erp = st.number_input("Equity Risk Premium (%)", value=5.0, min_value=0.0, max_value=10.0, step=0.1)
+        lm = st.number_input("Lenders Margin (%)", value=2.0, min_value=0.0, max_value=5.0, step=0.1)
+    with col4:
+        st.subheader("Financing Structure")
+        st.write("")
+        st.write("")
+        debt_share = st.number_input("Debt Share (%)", value=60, min_value=0, max_value=100, step=10)
+        tax_rate = st.number_input("Tax Rate (%)", value=25, min_value=0, max_value=50, step=1)
+    st.subheader("Comparison of Projected WACC with Historical Estimates")
+
+    # Evaluate projected data
+    projected_data = wacc_predictor.calculate_country_wacc(rf_rate, crp, tax_rate, technology, market_maturity, country_code, debt_share, erp, lm, tech_penetration, projection_year)
+    projected_data["Year"] = projection_year
+
+    # Extract historical data for the given country
+    selected_wacc = get_selected_country(yearly_waccs, country_code)
+    selected_wacc["Year"] = year
+
+    # Create a bar chart with historical, cost of equity, cost of debt, and overall wacc
+    evaluated_wacc_data = pd.concat([selected_wacc, projected_data])
+    evaluated_wacc_data = evaluated_wacc_data.drop(columns = ["Debt_Share", "Equity_Cost", "Debt_Cost", "Tax_Rate", "Country code", "WACC"])
+    st.write(evaluated_wacc_data)
+    plot_comparison_chart(evaluated_wacc_data)
+
 with tab4:
     text = open('about.md').read()
     st.write(text)
--- wacc_prediction.py
+++ wacc_prediction.py
@@ -1,9 +1,10 @@
 import xarray as xr
 import pandas as pd
 import numpy as np
+import streamlit as st
 
 class WaccPredictor:
-    def __init(self, country_categories, crp_data, generation_data, GDP, tax_data):
+    def __init__(self, crp_data, generation_data, GDP, tax_data, ir_data):
         """ Initialises the WACC Predictor Class, which is used to generate an estimate of the cost of capital at
          a national level for countries with available data
         
@@ -24,52 +25,243 @@ class WaccPredictor:
         self.generation_data = pd.read_csv(generation_data)
         self.gdp_data = pd.read_csv(GDP)
         self.tax_data = pd.read_csv(tax_data)
-        self.rf_rate = 2.6 # Add in historical calculation
+        self.ir_data = pd.read_csv(ir_data)
        
-        # Set up initial calculator
-        self.historical_waccs = self.calculate_historical_waccs()
-
-
-    def calculate_historical_waccs(self):
-
-        # Set up the storage dataframe for cost of equity and cost of debt
-        storage_df = pd.DataFrame(columns=["Country", "Country code", "Risk_Free_Rate", "CRP", 
-                                           "Renewable_Penetration", "Equity_Cost", 
-                                           "Debt_Cost", "Debt_Share", "Tax_Rate", "WACC"])
+        # Set up initial assumptions
+        self.lenders_margin = 2
         
-        # Add rows for countries with data 
-        storage_df[['Country', 'Country code']] = self.crp_data[['Country', 'Country code']]
 
-        # Create a multiindex of years and countries
-        years = self.crp_data.columns.values.to_list[2:-1]
-        storage_df = pd.concat([storage_df.assign(year=year) for year in years], ignore_index=True)
+    def calculate_historical_waccs(self, year, technology):
 
-        # Loop over each year
-        for year in years:
+        def fill_missing_RE_values(data, previous_year, year):
 
-            # Calculate Risk Free Rate
-            storage_df.loc[storage_df['year'] == year, "Risk_Free_Rate"] = self.rf_rate
-            rf_rate = storage_df.loc[storage_df['year'] == year]['Risk_Free_Rate']
+            # Set Country Code as index
+            data.set_index('Country code', inplace=True)
+            previous_year.set_index('Country code', inplace=True)
 
-            # Calculate Country Risk
-            storage_df.loc[storage_df['year'] == year, "CRP"] = self.crp_data[str(year)]
-            country_risk = storage_df.loc[storage_df['year'] == year]['CRP']
+            # Fill missing values for 2023 with 2022 data
+            data = pd.merge(data, previous_year, on="Country code", how="outer")
+            data['Penetration_' + str(year)] = data['Penetration_' + str(year)].fillna(data['Penetration_'+str(year-1)])
 
-            # Extract Equity Risk Premium
-            storage_df.loc[storage_df['year'] == year, "ERP"] 
+            # Reset index if needed
+            data.reset_index(inplace=True)
 
-            # Extract Lenders Margin
-            lenders_margin = self.lenders_margin
+            return data
+        
+        # Convert year into a string
+        year_str = str(year)
+        year_int = int(year)
 
-            # Extract Generation Data 
+        # Extract long term U.S. interest rates (proxy for risk free rate)
+        rf_rate = self.ir_data[self.ir_data['Country code'] == "USA"][year_str].values[0].astype(float)
 
-            # Calculate Technology Risk
+        # Extract CRPs
+        crps = self.pull_CRP_data(year_str)
+        erp = crps[crps['Country code']=="ERP"]["CRP_"+year_str][0]
+        crp_data = crps["CRP_"+year_str]
 
-            # Calculate Cost of Equity and Cost of Debt
+        # Extract Generation Data
+        if technology == "Solar PV":
+            ember_name = "Solar"
+        elif technology == "Onshore Wind":
+            ember_name = "Wind"
+        elif technology == "Offshore Wind":
+            ember_name = "Wind"
+        generation_data = self.pull_generation_data_v2(year_str, ember_name)
+        previous_year = self.pull_generation_data_v2(str(year_int-1), ember_name)
+        generation_data = fill_missing_RE_values(generation_data, previous_year, year_int)
 
-            # Estimate Debt Share
 
-            # Calculate Weighted Average Cost of Capital
+        # Extract Tax Rates
+        tax_rate = pd.merge(self.crp_data['Country code'], self.tax_data[['Country code', 'Tax_Rate']], on="Country code", how="left")
+        tax_rate['Tax_Rate'] = tax_rate['Tax_Rate'].fillna(value=0)
+        tax_data = tax_rate['Tax_Rate'].values.astype(float)
+                           
 
+        # Calculate WACC and contributions
+        results = self.calculate_irena_wacc(rf_rate=rf_rate, crp=crp_data, tax_rate=tax_data, technology=technology, erp=erp, lm=self.lenders_margin,
+                                            tech_penetration=generation_data['Penetration_'+year_str], country_codes=self.crp_data['Country code'])
+
+        return results
+
+    def pull_CRP_data(self, year):
+
+        
+        # Extract generation data
+        data = self.crp_data
+        
+        # Extract specific year
+        data_subset = data[["Country", "Country code", year]]
+        data_subset = data_subset.rename(columns={year: "CRP_"+year})
+        
+        
+        return data_subset
+    
+
+    def pull_generation_data_v2(self, year_str, technology):
+
+        
+        # Extract generation data
+        generation_data = self.generation_data
+        year = int(year_str)
+        
+        # Extract Capacity
+        capacity_subset = generation_data[(generation_data['Year'] == year) & (generation_data['Category'] == "Capacity") & (generation_data['Unit'] == "GW")]                                             
+        capacity_data = capacity_subset[capacity_subset['Variable'] == technology]
+        capacity_data = capacity_data.rename(columns = {"Value" : "Capacity_" + year_str, "YoY absolute change": "Capacity_" + year_str + "_YoY_Change"})
+
+        
+        # Extract Penetration
+        penetration_subset = generation_data[(generation_data['Year'] == year) & (generation_data['Category'] ==  "Electricity generation") & (generation_data['Unit'] == "%")]  
+        penetration_data = penetration_subset[penetration_subset['Variable'] == technology]
+        penetration_data = penetration_data.rename(columns = {"Value" : "Penetration_" + year_str, "YoY absolute change": "Penetration_" + year_str + "_YoY_Change"})
+
+        
+        # Extract needed data
+        penetration_data = penetration_data[["Area", "Country code", "Year", "Continent", "Penetration_" + year_str,"Penetration_" + year_str + "_YoY_Change"]]
+        capacity_data  = capacity_data[["Country code", "Capacity_" + year_str, "Capacity_" + year_str + "_YoY_Change"]]
+        data_for_output = pd.merge(penetration_data, capacity_data, on="Country code", how="outer")
+
+        # Extract only data that is present in the CRP dataset
+        data_for_output = pd.merge(self.crp_data['Country code'], data_for_output, how="left", on="Country code")
+        
+        return data_for_output
+
+
+    def calculate_country_wacc(self, rf_rate, crp, tax_rate, technology, market_maturity, country_code, debt_share=None, erp=None, lm=None, tech_penetration=None, year=None):
+        
+
+        
+        # Calculate maturity of market
+        if tech_penetration is not None:
+            if technology == "Onshore Wind" or "Solar PV":
+                if tech_penetration > 10:
+                    maturity = "Mature"
+                elif tech_penetration > 5:
+                    maturity = "Intermediate"
+                else: 
+                    maturity = "Immature"
+            else:
+                if tech_penetration > 6:
+                    maturity = "Mature"
+                elif tech_penetration > 3:
+                    maturity = "Intermediate"
+                else: 
+                    maturity = "Immature"
+        else:
+            maturity = market_maturity
+            
+
+
+        # Calculate technology premium
+        if maturity == "Mature":
+            technology_premium = 1.5
+        elif maturity == "Intermediate":
+            technology_premium = 2.375
+        else:
+            technology_premium = 3.25
+        
+        # Calculate debt share, if applicable
+        if debt_share is None:
+            if maturity == "Mature":
+                debt_share = 80
+            elif maturity == "Intermediate":
+                debt_share = 70
+            elif maturity == "Immature":
+                debt_share = 60
+
+        # Calculate the cost of equity
+        debt_cost = rf_rate + crp + lm + technology_premium
+
+        # Calculate the cost of debt
+        equity_cost = rf_rate + crp + erp + technology_premium
+
+        # Calculate the weighted average cost of capital
+        estimated_wacc = debt_cost * (debt_share/100) * (1 - (tax_rate/100)) + equity_cost * (1 - (debt_share/100))
+
+        # Extract contributions to the overall WACC
+        risk_free_contributions = rf_rate*((debt_share / 100 * (1 - tax_rate/100)) + (1 - debt_share / 100))
+        crp_contributions = crp*((debt_share / 100 * (1 - tax_rate/100)) + (1 - debt_share / 100))
+        erp_contributions = erp * ( 1 - (debt_share / 100))
+        lm_contributions = lm * (debt_share / 100) * (1-tax_rate/100)
+        tech_premium_contributions = technology_premium*((debt_share / 100 * (1 - tax_rate/100)) + (1 - debt_share / 100))
+        
+
+        # Include in a pandas dataframe
+        results_df = pd.DataFrame(data={"Country code": country_code, "Risk_Free":risk_free_contributions, "Country_Risk": crp_contributions, "Equity Risk": erp_contributions, "Lenders Margin": lm_contributions, 
+                                        "Technology_Risk": tech_premium_contributions, "Equity_Cost": equity_cost, "Debt_Cost": debt_cost, "WACC": estimated_wacc, "Debt_Share": debt_share, "Tax_Rate": tax_rate}, index=[str(year)])
+        
+        return results_df
+    
+
+    def calculate_irena_wacc(self, rf_rate, crp, tax_rate, technology, erp, lm, tech_penetration, country_codes):
+
+
+        # Calculate technology premium
+        def calculate_technology_premium(maturity):
+            premium = np.empty_like(maturity, dtype=float)
+            premium[maturity == "Mature"] = 1.5
+            premium[maturity == "Intermediate"] = 2.375
+            premium[maturity == "Immature"] = 3.25
+            return premium
+
+        # Calculate debt share
+        def calculate_debt_share(maturity):
+            debt_share = np.empty_like(maturity, dtype=float)
+            debt_share[maturity == "Mature"] = 80
+            debt_share[maturity == "Intermediate"] = 70
+            debt_share[maturity == "Immature"] = 60
+            return debt_share
+     
+        # Fill locations where tech penetration is empty with 
+        tech_penetration = np.where(np.isnan(tech_penetration), 0, tech_penetration)
+        tech_penetration = np.where(tech_penetration == None, 0, tech_penetration)
+
+
+        # Calculate maturity of market
+        maturity = np.empty_like(tech_penetration, dtype=object)
+        if technology in ["Onshore Wind", "Solar PV"]:
+            maturity[tech_penetration > 10] = "Mature"
+            maturity[(tech_penetration > 5) & (tech_penetration <= 10)] = "Intermediate"
+            maturity[tech_penetration <= 5] = "Immature"
+        else:
+            maturity[tech_penetration > 6] = "Mature"
+            maturity[(tech_penetration > 3) & (tech_penetration <= 6)] = "Intermediate"
+            maturity[tech_penetration <= 3] = "Immature"
+
+        # Calculate technology premium
+        technology_premium = calculate_technology_premium(maturity)
+
+        # Calculate debt share
+        debt_share = calculate_debt_share(maturity)
+
+        # Calculate the cost of equity
+        debt_cost = rf_rate + crp + lm + technology_premium
+
+        # Calculate the cost of debt
+        equity_cost = rf_rate + crp + erp + technology_premium
+
+        # Calculate the weighted average cost of capital
+        estimated_wacc = debt_cost * (debt_share/100) * (1 - (tax_rate/100)) + equity_cost * (1 - (debt_share/100))
+        
+        # For offshore wind, add a 1% premium
+        if technology == "Offshore Wind":
+            estimated_wacc = estimated_wacc + 1
+
+        # Extract contributions to the overall WACC
+        risk_free_contributions = rf_rate*((debt_share / 100 * (1 - tax_rate/100)) + (1 - debt_share / 100))
+        crp_contributions = crp*((debt_share / 100 * (1 - tax_rate/100)) + (1 - debt_share / 100))
+        erp_contributions = erp * ( 1 - (debt_share / 100))
+        lm_contributions = lm * (debt_share / 100) * (1-tax_rate/100)
+        tech_premium_contributions = technology_premium*((debt_share / 100 * (1 - tax_rate/100)) + (1 - debt_share / 100))
+
+        # Build Dataframe for results
+        results_df = pd.DataFrame(data={"Country code": country_codes, "Risk_Free":risk_free_contributions.round(decimals=2), "Country_Risk": crp_contributions.round(decimals=2), "Equity Risk": erp_contributions.round(decimals=2), "Lenders Margin": lm_contributions, 
+                                        "Technology_Risk": tech_premium_contributions.round(decimals=2), "Equity_Cost": equity_cost.round(decimals=2), "Debt_Cost": debt_cost.round(decimals=2), "WACC": estimated_wacc.round(decimals=2), "Debt_Share": debt_share, "Tax_Rate": tax_rate})
+        results_df = results_df.tail(-1)
+
+        return results_df
+
+        
 
         
